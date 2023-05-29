b5ca2cc371892653346e39373cc771c1acbd58f1
Shubham <shubhamvaarshney735@gmail.com>
2023-05-29 14:14:35 +0530

    update

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ import streamlit as st
 import streamlit.components.v1 as components
 from rdkit import Chem
 from rdkit.Chem.Descriptors import ExactMolWt, MolLogP, NumHDonors, NumHAcceptors
+import deepchem as dc
+import numpy as np
+from deepchem.models import GraphConvModel
 
-st.title("Filter FDA Approved Drugs by Lipinski's Rule-of-Five with Streamlit")
+
+
+st.title("Drug Discovery Project")
 
 st.markdown("""
+- by BBDU students
 """)
 
 @st.cache_data()
@@ -43,6 +49,15 @@ def calc_NumHAcceptors(smiles_string):
     mol = Chem.MolFromSmiles(smiles_string)
     return NumHAcceptors(mol)
 
+model = None
+with st.spinner("loading toxicity detection model"):
+    # Load Tox21 dataset
+    tox21_tasks, tox21_datasets, transformers = dc.molnet.load_tox21(featurizer='GraphConv')
+    train_dataset, valid_dataset, test_dataset = tox21_datasets
+    # Training the Graph Convolution Network
+    model = GraphConvModel(
+        len(tox21_tasks), batch_size=10, mode='classification')
+    model.restore(model_dir='model')
 
 # Copy the dataset so any changes are not applied to the original cached version
 df = download_dataset().copy()
@@ -100,3 +115,16 @@ raw_html = mols2grid.display(df_result4,
                             style={"__all__": lambda x: "color: red" if x["cns_drug"] < -5 else ""}
                            )._repr_html_()
 components.html(raw_html, width=1200, height=1100, scrolling=False)
+
+if st.sidebar.button("Display Toxicity Prediction"):
+    results = model.predict(train_dataset) 
+    st.sidebar.write(results[0])
+    st.sidebar.markdown('''
+    Evaluating model training results:
+    
+    Training ROC-AUC Score: 0.951326
+    
+    Validation ROC-AUC Score: 0.732432
+    
+    Test ROC-AUC Score: 0.709457
+    ''')
